8616d2021cccb1da398c5269dcd21b4611216b59
Athif <Athif045@gmail.com>
2022-06-01 11:13:26 +0530

    inital commit

---
+++ src/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from './logo.png'
+import './sidebar.css'
+
+function Sidebar() {
+  return (
+    <div>
+        <img src={Logo}/>
+    </div>
+  )
+}
+
+export default Sidebar;
